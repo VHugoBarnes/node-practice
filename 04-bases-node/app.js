774563b1aaac9857a387ad18c833a466e8d74e00
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-08 20:47:27 -0600

    yargs

--- 04-bases-node/app.js
+++ 04-bases-node/app.js
@@ -1,12 +1,24 @@
+const argv = require('yargs')
+                    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+                        base: {
+                            demand: true,
+                            alias: 'b'
+                        },
+                        limite: {
+                            alias: 'l',
+                            default: 10
+                        }
+                    })
+                    .help()
+                    .argv;
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-// console.log(process.argv);
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
 
-console.log(base);
+let argv2 = process.argv;
 
-crearArchivo( base )
-    .then( archivo => console.log(`Archivo creado: ${archivo}`))
-    .catch( err => console.log(err));
+console.log(argv.base);
+console.log(argv.limite);
+
+// crearArchivo( base )
+//     .then( archivo => console.log(`Archivo creado: ${archivo}`))
+//     .catch( err => console.log(err));
