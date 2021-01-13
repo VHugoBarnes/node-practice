00fd0b9e2c7e40571d56cd9e5290a4bb32824586
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-12 23:03:02 -0600

    Peticiones HTTP con Axios

--- 06-clima-mundo/app.js
+++ 06-clima-mundo/app.js
@@ -1,3 +1,4 @@
+const axios = require('axios').default;
 const { argv } = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -5,5 +6,22 @@ const { argv } = require('yargs').options({
         demand: true
     }
 });
+const { API_KEY } = require('./secrets');
 
+const instance = axios.create({
+    baseURL:'http://api.weatherapi.com/v1/current.json',
+    params: {
+        q: `${ argv.direccion }`
+    },
+    headers: {
+        key: API_KEY
+    }
+});
 console.log(argv.direccion);
+instance.get()
+    .then( resp => {
+        console.log(resp.data.current.temp_c);
+    })
+    .catch( err => {
+        console.log(err);
+    });
