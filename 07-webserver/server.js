679085e77714f7ae493c4b26932f7107c1e82fa8
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-14 16:40:06 -0600

    Handlebars

--- 07-webserver/server.js
+++ 07-webserver/server.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.use( express.static(__dirname + '/public') )
+app.use( express.static(__dirname + '/public') );
+// Express HBS
+app.set('view engine', 'hbs');
  
-// app.get('/', (req, res) => {
+app.get('/', (req, res) => {
 
-//     //   res.send('Hello World');
-//     let salida = {
-//         nombre: 'Víctor',
-//         edad: 21,
-//         url: req.url
-//     }
+    res.render('home', {
+        nombre: 'Víctor'
+    });
 
-//     res.send(salida);
-
-// });
+});
 
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
