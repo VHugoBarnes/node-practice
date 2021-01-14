5f12c4927c0e3a58e8684952f73ad069414b3adf
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-13 22:20:18 -0600

    Introducción a Express

---
+++ 07-webserver/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+
+    //   res.send('Hello World');
+    let salida = {
+        nombre: 'Víctor',
+        edad: 21,
+        url: req.url
+    }
+
+    res.send(salida);
+
+});
+
+app.listen(3000, () => {
+    console.log('Escuchando peticiones en el puerto 3000');
+});
