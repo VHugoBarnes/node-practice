dca7ec7562507dacdcada36cc83e6468010af34d
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-16 17:59:29 -0600

    Mongoose - Conectarnos a la base de datos

--- 08-rest-server/server/server.js
+++ 08-rest-server/server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+const app = express();
 require('./config/config');
 
 // parse application/x-www-form-urlencoded
@@ -47,6 +49,17 @@ app.delete('/usuario', function (req, res) {
     res.json('delete Usuario');
 });
  
+mongoose.connect('mongodb://localhost:27017/cafe',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  } ,(err, res) => {
+    if(err) throw err;
+
+    console.log('Base de datos online');
+});
+
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto: ', process.env.PORT);
 });
