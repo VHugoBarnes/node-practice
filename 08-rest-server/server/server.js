1d36d7a51b6d8da01d1e17747effd87e32daaba9
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-15 21:01:49 -0600

    Manejando peticiones HTTP en nuestro rest server

--- 08-rest-server/server/server.js
+++ 08-rest-server/server/server.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+// parse application/json
+app.use(bodyParser.json())
+ 
+// Servicio rest
+app.get('/usuario', function (req, res) {
+    res.json('get Usuario');
+});
+ 
+// Servicio post
+app.post('/usuario', function (req, res) {
+    let body = req.body;
+    res.json({
+        body
+    });
+});
+ 
+// Servicio put
+app.put('/usuario/:id', function (req, res) {
+    
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+ 
+// Servicio delete
+app.delete('/usuario', function (req, res) {
+    res.json('delete Usuario');
 });
  
 app.listen(3000, () => {
