bffae093e16d218c69f6528354d0e11a31264aab
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-16 19:05:54 -0600

    Validaciones personalizadas - email y role

--- 08-rest-server/server/models/usuario.js
+++ 08-rest-server/server/models/usuario.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 let Schema = mongoose.Schema;
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+}
 
 let usuarioSchema = new Schema({
     nombre: {
@@ -10,6 +16,7 @@ let usuarioSchema = new Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: [true, 'El correo es necesario']
     },
     password: {
@@ -21,7 +28,8 @@ let usuarioSchema = new Schema({
     },
     role: { // Default 'USER_ROLE'
         type: String,
-        default: 'USER_ROLE'
+        default: 'USER_ROLE',
+        enum: rolesValidos
     },
     estado: { // Boolean
         type: Boolean,
@@ -32,5 +40,9 @@ let usuarioSchema = new Schema({
         default: false
     },
 });
+
+usuarioSchema.plugin( uniqueValidator, {
+    message: '{PATH} debe de ser único'
+} );
 
 module.exports = mongoose.model('Usuario', usuarioSchema)
