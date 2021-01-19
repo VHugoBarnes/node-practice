8a2cb37b8578cd6ef38710b69a1a177f5cdccf6e
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-19 16:21:13 -0600

    Generando un JWT

--- 08-rest-server/server/config/config.js
+++ 08-rest-server/server/config/config.js
@@ -1,2 +1,9 @@
 // Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Vencimiento del token
+// 60 segs * 60 mins * 24 hrs * 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// Seed del token
+process.env.SEED = process.env.SEED || 'este-es-el-seed-de-desarrollo';
