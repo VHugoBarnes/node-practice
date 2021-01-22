7927f2896098462fade9e2986a612eccfa6b7451
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-22 16:39:35 -0600

    Validar la extensión del archivo a subir

--- 08-rest-server/server/routes/upload.js
+++ 08-rest-server/server/routes/upload.js
@@ -19,8 +19,25 @@ app.put('/upload', (req, res) => {
         });
     }
 
+    // Obtener el archivo de la request
     let archivo = req.files.archivo;
+    // El path donde estará guardada la información
     const filePath = path.join(__dirname + '../../../uploads/') + archivo.name;
+    // Restringir la extensión del archivo a subir
+    let extensionesValidas = ['png', 'gif', 'jpg', 'jpeg'];
+    // Obtener la extensión del archivo
+    let nombreArchivo = archivo.name.split('.');
+    let extensionArchivo = nombreArchivo[nombreArchivo.length - 1]; // Obtener lo que viene después del punto
+
+    if(extensionesValidas.indexOf(extensionArchivo) < 0) {
+        return res.status(406).json({
+            ok: false,
+            err: {
+                message: 'Extensión no valida, solo se permite ' + extensionesValidas.join(', '),
+                ext: extensionArchivo,
+            }
+        });
+    }
 
     archivo.mv(filePath, (err) => {
 
