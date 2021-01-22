0fe2de796c51cd94ea6e17c5e2acec2dc658fe9d
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-22 17:29:33 -0600

    Ubicando y renombrando archivos

--- 08-rest-server/server/routes/upload.js
+++ 08-rest-server/server/routes/upload.js
@@ -3,13 +3,19 @@ const path = require('path');
 const fileUpload = require('express-fileupload');
 const { sample } = require('underscore');
 
+const Usuario = require('../models/usuario');
 const app = express();
 
 // Default options
-app.use(fileUpload({useTempFiles : true}));
+app.use(fileUpload());
 
-app.put('/upload', (req, res) => {
+app.put('/upload/:tipo/:id', (req, res) => {
 
+    // Recolectar datos de los params
+    let tipo = req.params.tipo;
+    let id = req.params.id;
+
+    // Si no se mandó ningún archivo
     if(!req.files) {
         return res.status(400).json({
             ok: false,
@@ -19,16 +25,28 @@ app.put('/upload', (req, res) => {
         });
     }
 
+    // Validar tipo
+    let tiposValidos = ['productos', 'usuarios'];
+    if(tiposValidos.indexOf(tipo) <0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Los tipos permitidos son ' + tiposValidos.join(', '),
+                tipo
+            }
+        });
+    }
+
     // Obtener el archivo de la request
     let archivo = req.files.archivo;
-    // El path donde estará guardada la información
-    const filePath = path.join(__dirname + '../../../uploads/') + archivo.name;
+
     // Restringir la extensión del archivo a subir
     let extensionesValidas = ['png', 'gif', 'jpg', 'jpeg'];
     // Obtener la extensión del archivo
-    let nombreArchivo = archivo.name.split('.');
-    let extensionArchivo = nombreArchivo[nombreArchivo.length - 1]; // Obtener lo que viene después del punto
+    let nombreSeparado = archivo.name.split('.');
+    let extensionArchivo = nombreSeparado[nombreSeparado.length - 1]; // Obtener lo que viene después del punto
 
+    // Validar si la extensión es valida
     if(extensionesValidas.indexOf(extensionArchivo) < 0) {
         return res.status(406).json({
             ok: false,
@@ -39,6 +57,13 @@ app.put('/upload', (req, res) => {
         });
     }
 
+     // El path donde estará guardada la información
+     let ruta = `../../../uploads/${tipo}/`;
+     // Cambiar el nombre del archivo
+     const nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extensionArchivo}`;
+     const filePath = path.join(__dirname + ruta) + nombreArchivo;
+
+    // Guardar el archivo
     archivo.mv(filePath, (err) => {
 
         if(err) {
