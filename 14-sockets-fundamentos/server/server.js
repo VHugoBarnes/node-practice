ffe6a4254179e00cc6b835b0b28a04cf31d47403
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-23 17:51:29 -0600

    Retroalimentación del cliente hacia el servidor

--- 14-sockets-fundamentos/server/server.js
+++ 14-sockets-fundamentos/server/server.js
@@ -17,13 +17,28 @@ io.on('connection', (client) => {
 
     console.log('Usuario conectado');
 
+    client.emit('enviarMensaje', {
+        usuario: 'Administrador',
+        mensaje: 'Bienvenido'
+    });
+
     client.on('disconnect', () => {
         console.log('Usuario desconectado');
     });
 
     // Escuchar al cliente
-    client.on('enviarMensaje', (mensaje) => {
-        console.log(mensaje);
+    client.on('enviarMensaje', (mensaje, callback) => {
+        // console.log(mensaje);
+
+        if(mensaje.nombre) {
+            callback({
+                res: 'Todo salió bien'
+            });
+        } else {
+            callback({
+                res: 'Todo salió bien'
+            });
+        }
     });
 
 });
