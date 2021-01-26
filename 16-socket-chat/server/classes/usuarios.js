0b670f3a125a8a4be1cbb3e4894e9c1237f830f2
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-26 14:27:24 -0600

    Clase para controlar los usuarios del chat

---
+++ 16-socket-chat/server/classes/usuarios.js
@@ -0,0 +1,64 @@
+
+/**
+ * El objeto de Usuarios quedaría algo así:
+ * 
+ * {
+ *      id: 'ssdasSASfxcht-44f3d',
+ *      nombre: 'Nicole',
+ *      sala: 'Sailor Moon'
+ * }
+ */
+
+class Usuarios {
+
+    constructor() {
+        this.personas = [];
+    }
+
+    agregarPersona(id, nombre) {
+        // Creamos a la persona conforme a lo pasado por parámetro
+        let persona = { id, nombre };
+        // Agregamos a la persona en la lista de personas
+        this.personas.push(persona);
+
+        return this.personas;
+
+    }
+
+    obtenerPersona(id) {
+
+        // Hacemos un filtro por id del arreglo de personas del chat,
+        // creo que obtener el indice al final es un poco redundante, puesto
+        // que el id es único
+        let persona = this.personas.filter( persona => persona.id === id )[0];
+
+        return persona;
+
+    }
+
+    obtenerPersonas() {
+        return this.personas;
+    }
+
+    obtenerPersonasPorSala(sala) {
+        // ToDo: Implementar la salas
+    }
+
+    borrarPersona(id) {
+
+        // Antes de borrar la relación de la lista obtenemos el objeto
+        let personaBorrada = this.obtenerPersona(id);
+        
+        // Hacemos un filtro que nos devuelva a todas las personas excepto a la
+        // del id.
+        this.personas = this.personas.filter(persona => persona.id !== id);
+
+        return personaBorrada;
+
+    }
+
+}
+
+module.exports = {
+    Usuarios
+};
